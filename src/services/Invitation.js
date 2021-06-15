f65a5a845fe8f06a5a06ffe6cf56a38a92c112a3
zodaland <ekwhwhek@gmail.com>
2021-06-15 17:08:04 +0900

    [services] console 함수 제거

--- src/services/Invitation.js
+++ src/services/Invitation.js
@@ -3,7 +3,6 @@ import axios from '../library/axios'
 export function create(params) {
 	return axios.post('/invitation', params)
 	.then((response) => {
-		console.log(response)
 		return response
 	})
 }
--- src/services/Room.js
+++ src/services/Room.js
@@ -3,7 +3,6 @@ import axios from '../library/axios'
 export function create() {
 	return axios.post('/room')
 	.then((response) => {
-		console.log(response)
 		return response
 	})
 }
